e2ed01e6abba6ccfb61f5323bb3b0e1860c72617
Jan Dostal <jan.dostal@tul.cz>
2023-05-09 22:00:40 +0200

    Added Episode data class

---
+++ star-wars-media-content-management/src/main/java/app/models/data/Episode.java
@@ -0,0 +1,32 @@
+
+package app.models.data;
+
+import java.time.Duration;
+
+/**
+ *
+ * @author jan.dostal
+ */
+public class Episode extends StarWarsMediaContent
+{
+    private int orderInTvShowSeason;
+    
+    public Episode(int id, Duration runtime, String name,
+            int percentageRating, boolean wasWatched, String hyperlinkForContentWatch,
+            String shortContentSummary, int orderInTvShowSeason) 
+    {
+        super(id, runtime, name, percentageRating, wasWatched, hyperlinkForContentWatch,
+                shortContentSummary);
+        this.orderInTvShowSeason = orderInTvShowSeason;
+    }
+
+    public int getOrderInTvShowSeason() 
+    {
+        return orderInTvShowSeason;
+    }
+
+    protected @Override String getSubclassInstanceDataAttributesValues() 
+    {
+        return ", orderInTvShowSeason=" + orderInTvShowSeason;
+    }
+}
