ab20e6b198ae7e394fbfd2a58a32e910499b7ec2
Jan Dostal <jan.dostal@tul.cz>
2023-05-09 21:11:27 +0200

    Added StarWarsMediaContent abstract class

---
+++ star-wars-media-content-management/src/main/java/app/models/data/StarWarsMediaContent.java
@@ -0,0 +1,87 @@
+
+package app.models.data;
+
+import java.time.Duration;
+
+/**
+ *
+ * @author jan.dostal
+ */
+public abstract class StarWarsMediaContent 
+{
+    private int id;
+    
+    private Duration runtime;
+    
+    private String name;
+    
+    private int percentageRating;
+    
+    private boolean wasWatched;
+    
+    private String hyperlinkForContentWatch;
+    
+    private String shortContentSummary;
+    
+    protected StarWarsMediaContent(int id, Duration runtime, String name,
+            int percentageRating, boolean wasWatched, String hyperlinkForContentWatch,
+            String shortContentSummary) 
+    {
+        this.id = id;
+        this.runtime = runtime;
+        this.name = name;
+        this.percentageRating = percentageRating;
+        this.wasWatched = wasWatched;
+        this.hyperlinkForContentWatch = hyperlinkForContentWatch;
+        this.shortContentSummary = shortContentSummary;
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public Duration getRuntime() 
+    {
+        return runtime;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public int getPercentageRating() 
+    {
+        return percentageRating;
+    }
+
+    public boolean isWasWatched() 
+    {
+        return wasWatched;
+    }
+
+    public String getHyperlinkForContentWatch() 
+    {
+        return hyperlinkForContentWatch;
+    }
+
+    public String getShortContentSummary() 
+    {
+        return shortContentSummary;
+    }
+    
+    protected abstract String getSubclassInstanceTypeName();
+    
+    protected abstract String getSubclassInstanceDataAttributesValues();
+    
+    public @Override String toString() 
+    {
+        return getSubclassInstanceTypeName() + "{id=" + id + ", runtime=" + 
+                runtime.toMinutes() + ", name=" + name + ", percentageRating=" +
+                percentageRating + " %, wasWatched=" + wasWatched +
+                ", hyperlinkForContentWatch=" + hyperlinkForContentWatch +
+                ", shortContentSummary=" + shortContentSummary + 
+                getSubclassInstanceDataAttributesValues() + "}";
+    }
+}
