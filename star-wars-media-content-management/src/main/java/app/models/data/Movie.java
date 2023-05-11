cf96a7b036ebc44fe72f8c8bb55b4b6995c55c4e
Jan Dostal <jan.dostal@tul.cz>
2023-05-11 07:32:59 +0200

    Added Era enum data class

--- star-wars-media-content-management/src/main/java/app/models/data/Movie.java
+++ star-wars-media-content-management/src/main/java/app/models/data/Movie.java
@@ -8,26 +8,34 @@ import java.time.LocalDate;
  *
  * @author jan.dostal
  */
-public class Movie extends StarWarsMediaContent
+public class Movie extends MediaContent
 {
     private LocalDate releaseDate;
     
+    private Era era;
+    
     public Movie(int id, Duration runtime, String name,
             int percentageRating, boolean wasWatched, String hyperlinkForContentWatch,
-            String shortContentSummary, LocalDate releaseDate) 
+            String shortContentSummary, LocalDate releaseDate, Era era) 
     {
         super(id, runtime, name, percentageRating, wasWatched, hyperlinkForContentWatch,
                 shortContentSummary);
-        this.releaseDate = releaseDate  ;
+        this.releaseDate = releaseDate;
+        this.era = era;
     }
     
     public LocalDate getReleaseDate() 
     {
         return releaseDate;
     }
+    
+    public Era getEra() 
+    {
+        return era;
+    }
 
     protected @Override String getSubclassInstanceDataAttributesValues() 
     {
-        return ", releaseDate=" + releaseDate;
+        return ", releaseDate=" + releaseDate.toString() + ", era=" + era.toString();
     }
 }
