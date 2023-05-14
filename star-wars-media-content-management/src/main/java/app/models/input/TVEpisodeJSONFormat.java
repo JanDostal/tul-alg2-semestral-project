d52833e79ddfb914c94f22b99627c61f3ca11f6d
Jan Dostal <jan.dostal@tul.cz>
2023-05-15 00:50:39 +0200

    Removed foreign key from TVEpisodeJSONFormat Class

--- star-wars-media-content-management/src/main/java/app/models/input/TVEpisodeJSONFormat.java
+++ star-wars-media-content-management/src/main/java/app/models/input/TVEpisodeJSONFormat.java
@@ -9,32 +9,23 @@ package app.models.input;
 public class TVEpisodeJSONFormat extends MediaContentJSONFormat
 {
     private int orderInTVShowSeason;
-    
-    private int tvSeasonId;
-    
+        
     public TVEpisodeJSONFormat(String runtime, String name,
             int percentageRating, boolean wasWatched, String hyperlinkForContentWatch,
-            String shortContentSummary, int orderInTVShowSeason, int tvSeasonId) 
+            String shortContentSummary, int orderInTVShowSeason) 
     {
         super(runtime, name, percentageRating, wasWatched, hyperlinkForContentWatch,
                 shortContentSummary);
         this.orderInTVShowSeason = orderInTVShowSeason;
-        this.tvSeasonId = tvSeasonId;
     }
 
     public int getOrderInTVShowSeason() 
     {
         return orderInTVShowSeason;
     }
-    
-    public int getTVSeasonId() 
-    {
-        return tvSeasonId;
-    }
 
     protected @Override String getSubclassInstanceDataAttributesValues() 
     {
-        return ", orderInTVShowSeason=" + orderInTVShowSeason + ", tvSeasonId=" +
-                tvSeasonId;
+        return ", orderInTVShowSeason=" + orderInTVShowSeason;
     }
 }
