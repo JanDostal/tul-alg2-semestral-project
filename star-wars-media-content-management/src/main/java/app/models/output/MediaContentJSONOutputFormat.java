bcc0afaf142012893ef70b392d8b593ee58a9a6b
Jan Dostal <jan.dostal@tul.cz>
2023-05-15 01:55:40 +0200

    Completed MediaContentJSONOutputFormat File JSON Class

---
+++ star-wars-media-content-management/src/main/java/app/models/output/MediaContentJSONOutputFormat.java
@@ -0,0 +1,89 @@
+
+package app.models.output;
+
+import java.time.Duration;
+
+/**
+ *
+ * @author jan.dostal
+ */
+public abstract class MediaContentJSONOutputFormat 
+{
+    private int id;
+    
+    private String runtime;
+    
+    private String name;
+    
+    private int percentageRating;
+    
+    private boolean wasWatched;
+    
+    private String hyperlinkForContentWatch;
+    
+    private String shortContentSummary;
+    
+    protected MediaContentJSONOutputFormat(int id, String runtime, String name,
+            int percentageRating, boolean wasWatched, String hyperlinkForContentWatch,
+            String shortContentSummary) 
+    {
+        this.id = id;
+        this.runtime = runtime;
+        this.name = name;
+        this.percentageRating = percentageRating;
+        this.wasWatched = wasWatched;
+        this.hyperlinkForContentWatch = hyperlinkForContentWatch;
+        this.shortContentSummary = shortContentSummary;
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public String getRuntime() 
+    {
+        return runtime;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public int getPercentageRating() 
+    {
+        return percentageRating;
+    }
+
+    public boolean getWasWatched() 
+    {
+        return wasWatched;
+    }
+
+    public String getHyperlinkForContentWatch() 
+    {
+        return hyperlinkForContentWatch;
+    }
+
+    public String getShortContentSummary() 
+    {
+        return shortContentSummary;
+    }
+    
+    protected String getSubclassInstanceTypeName() 
+    {
+        return this.getClass().getSimpleName();
+    }
+    
+    protected abstract String getSubclassInstanceDataAttributesValues();
+    
+    public @Override String toString() 
+    {   
+        return getSubclassInstanceTypeName() + "{id=" + id + ", runtime=" + 
+                runtime + ", name=" + name + ", percentageRating=" +
+                percentageRating + ", wasWatched=" + wasWatched +
+                ", hyperlinkForContentWatch=" + hyperlinkForContentWatch + 
+                getSubclassInstanceDataAttributesValues() + "}";
+    }
+}
