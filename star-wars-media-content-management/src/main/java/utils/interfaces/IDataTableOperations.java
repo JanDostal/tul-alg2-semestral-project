7ac2f9e080d72697cdff228a3ff3db09183380b6
Jan Dostal <jan.dostal@tul.cz>
2023-05-15 10:37:43 +0200

    Completed IDataTableOperations Generic Interface

--- star-wars-media-content-management/src/main/java/utils/interfaces/IDataTableOperations.java
+++ star-wars-media-content-management/src/main/java/utils/interfaces/IDataTableOperations.java
@@ -1,11 +1,18 @@
 
 package utils.interfaces;
 
+import java.util.List;
+
 /**
  *
  * @author jan.dostal
  */
-public interface IDataTableOperations<T>
+public interface IDataTableOperations<T, S, U, V>
 {
-    
+    T addInput(S inputData);
+    T addOutput(U outputData);
+    void delete(V primaryKey);
+    boolean edit(T data);
+    T getBy(V primaryKey);
+    List<T> getAll();
 }
