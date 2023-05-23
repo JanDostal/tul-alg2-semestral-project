9c75cfb34fc6afccb4ee7d3865d73ad6ffd11391
Jan Dostal <jan.dostal@tul.cz>
2023-05-23 19:03:00 +0200

    Improved interface IDataTable signatures

--- star-wars-media-content-management/src/main/java/utils/interfaces/IDataTable.java
+++ star-wars-media-content-management/src/main/java/utils/interfaces/IDataTable.java
@@ -1,18 +1,23 @@
 
 package utils.interfaces;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  *
  * @author jan.dostal
  */
-public interface IDataTable<T, S, U, V>
+public interface IDataTable<T extends Comparable<T>, Object, S extends Object, U extends Object, V>
 {
-    T addInput(S inputData);
-    T addOutput(U outputData);
-    void delete(V primaryKey);
-    boolean edit(V primaryKey, S inputData);
+    T addFrom(S inputData);
+    T loadFrom(U outputData);
+    void deleteBy(V primaryKey);
+    boolean editBy(V primaryKey, S inputData);
     T getBy(V primaryKey);
     List<T> getAll();
+    List<T> filterBy(Predicate<T> condition, List<T> sourceList);
+    void sortBy(Comparator<T> comparator, List<T> sourceList);
+    void sortByPrimaryKey(List<T> sourceList);
 }
