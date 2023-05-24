ded722ed7c082cee50f8b0efbdf483ba19de0e40
Jan Dostal <jan.dostal@tul.cz>
2023-05-24 03:16:16 +0200

    implemented interface IDataTable onto TvEpisodesTable

--- star-wars-media-content-management/src/main/java/app/logic/datacontext/TVEpisodesTable.java
+++ star-wars-media-content-management/src/main/java/app/logic/datacontext/TVEpisodesTable.java
@@ -1,11 +1,57 @@
 
 package app.logic.datacontext;
 
+import app.models.data.TVEpisode;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import utils.interfaces.IDataTable;
+
 /**
  *
  * @author jan.dostal
  */
-public class TVEpisodesTable 
+public class TVEpisodesTable implements IDataTable<TVEpisode>
 {
+
+    @Override
+    public TVEpisode addFrom(TVEpisode inputData) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void loadFrom(TVEpisode outputData) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteBy(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean editBy(int id, TVEpisode inputData) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TVEpisode getBy(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<TVEpisode> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<TVEpisode> filterBy(Predicate<TVEpisode> condition) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void sortBy(Comparator<TVEpisode> comparator, List<TVEpisode> sourceList) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
