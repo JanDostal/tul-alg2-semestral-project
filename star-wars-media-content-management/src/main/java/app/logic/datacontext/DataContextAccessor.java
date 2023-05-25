0bde48891d7559baa375f9536ba8c4ff08805ae7
Jan Dostal <jan.dostal@tul.cz>
2023-05-25 12:16:24 +0200

    New addition to data context accessor class

--- star-wars-media-content-management/src/main/java/app/logic/datacontext/DataContextAccessor.java
+++ star-wars-media-content-management/src/main/java/app/logic/datacontext/DataContextAccessor.java
@@ -1,11 +1,45 @@
 
 package app.logic.datacontext;
 
+import app.models.data.TVSeason;
+import app.models.data.TVShow;
+import utils.interfaces.IDataTable;
+
 /**
  *
  * @author jan.dostal
  */
 public class DataContextAccessor 
 {
+    private static DataContextAccessor dataContextAccessor;
     
+    private IDataTable<TVSeason> tvSeasonsTable;
+    
+    private IDataTable<TVShow> tvShowsTable;
+    
+    private DataContextAccessor() 
+    {
+        this.tvSeasonsTable = TVSeasonsTable.getInstance();
+        this.tvShowsTable = TVShowsTable.getInstance();
+    }
+
+    public IDataTable<TVSeason> getTvSeasonsTable() 
+    {
+        return tvSeasonsTable;
+    }
+
+    public IDataTable<TVShow> getTvShowsTable() 
+    {
+        return tvShowsTable;
+    }
+    
+    public static DataContextAccessor getInstance() 
+    {
+        if (dataContextAccessor == null) 
+        {
+            dataContextAccessor = new DataContextAccessor();
+        }
+        
+        return dataContextAccessor;
+    }
 }
