8de9c77a971ab5a2bda133532cfe6524d010dd39
Jan Dostal <jan.dostal@tul.cz>
2023-05-25 12:13:42 +0200

    DataModelsTest extended with tvShow equality test

--- star-wars-media-content-management/src/main/java/tests/mainmethods/DataModelsTest.java
+++ star-wars-media-content-management/src/main/java/tests/mainmethods/DataModelsTest.java
@@ -90,5 +90,46 @@ public class DataModelsTest {
         
         Movie movieNull = new Movie(null, null, null, 2, true, null, null, null, null);
         System.out.println(movieNull);
+        
+        //tvShow equality test
+        System.out.println();
+        System.out.println("tvShow equality test");
+        System.out.println();
+        
+        //same reference
+        TVShow show01 = new TVShow(new PrimaryKey(10), "show1", LocalDate.parse("2023-05-20", 
+                DateTimeFormatter.ISO_LOCAL_DATE), Era.AGE_OF_THE_REBELLION);
+        
+        TVShow show02 = show01;
+        
+        System.out.println("is same reference: show01 == show02: " + show01.equals(show02));
+        
+        //instances originating from same class
+        Movie movie01 = new Movie(new PrimaryKey(2), Duration.ofMinutes(45), "filmA", 
+                50, false, "https://www.example01.com", "Velmi krásný film", 
+                LocalDate.parse("2023-05-11", DateTimeFormatter.ISO_LOCAL_DATE), Era.FALL_OF_THE_JEDI);
+        
+        System.out.println("are instances from same class: show01 == movie01: " + show01.equals(movie01));
+        
+        
+        //same data
+        TVShow show03 = new TVShow(new PrimaryKey(10), "show2", LocalDate.parse("2023-05-20", 
+                DateTimeFormatter.ISO_LOCAL_DATE), Era.THE_NEW_REPUBLIC);
+        
+        System.out.println("are same data: show01 == show03: " + show01.equals(show03));
+        
+        //comparison if method decides origin of instances from data type or instances themselves
+        TVShow show04 = new TVShow(new PrimaryKey(10), "show1", LocalDate.parse("2023-05-20", 
+                DateTimeFormatter.ISO_LOCAL_DATE), Era.AGE_OF_THE_REBELLION);
+        DatabaseRecord show05 = show04;
+        
+        if (show01.equals(show05) == true) 
+        {
+            System.out.println("Instances show01 and show05 are compared by instances");
+        }
+        else 
+        {
+            System.out.println("Instances show01 and show05 are compared by data types");
+        }        
     }
 }
