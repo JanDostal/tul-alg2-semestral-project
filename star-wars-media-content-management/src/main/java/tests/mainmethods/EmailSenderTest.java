0f03e87374eb9aab6c0adbf99167bd4206a620c2
Jan Dostal <jan.dostal@tul.cz>
2023-05-28 09:08:30 +0200

    Implemented successfully email sender + done testing

---
+++ star-wars-media-content-management/src/main/java/tests/mainmethods/EmailSenderTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tests.mainmethods;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+
+/**
+ *
+ * @author Admin
+ */
+public class EmailSenderTest 
+{
+    public static void main(String[] args) throws EmailException 
+    {   
+        String randomGeneratedAppToken = "qnaadtxcznjyvzln";
+        String appId = "honzaswtor";
+        String enteredEmailAddress = "honzaswtor@gmail.com";
+        
+        try 
+        {
+            HtmlEmail email = new HtmlEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator(appId, randomGeneratedAppToken));
+            email.setSSLOnConnect(true);
+            email.setFrom(enteredEmailAddress);
+            email.setSubject("Star Wars Media Content Management - Test Email");
+            email.addTo(enteredEmailAddress);
+            email.setHtmlMsg("<html><a href=\"ahoj\">ahoj</a></html>");
+            email.send();  
+       }
+       catch (EmailException ex) 
+       {
+            System.out.println("Chyba sítě nebo neexistující zadaná emailová adresa");
+       }
+    }
+}
