77d69497fc641d40885192b28605f8e7080186d8
Jan Dostal <jan.dostal@tul.cz>
2023-06-03 11:19:21 +0200

    Completed TVSeasonDataConverter

--- star-wars-media-content-management/src/main/java/utils/helpers/TVSeasonDataConverter.java
+++ star-wars-media-content-management/src/main/java/utils/helpers/TVSeasonDataConverter.java
@@ -4,10 +4,49 @@
  */
 package utils.helpers;
 
+import app.models.data.PrimaryKey;
+import app.models.data.TVSeason;
+import app.models.input.TVSeasonInput;
+import app.models.output.TVSeasonOutput;
+
 /**
  *
  * @author Admin
  */
-public final class TVSeasonDataConverter {
+public final class TVSeasonDataConverter 
+{
+    private TVSeasonDataConverter()
+    {
+    }
     
+    public static TVSeasonOutput convertToOutputDataFrom(TVSeason data) 
+    {
+        int id = data.getPrimaryKey().getId();
+        
+        int orderInTVShow = data.getOrderInTVShow();
+        
+        int tvShowId = data.getTVShowForeignKey().getId();
+        
+        return new TVSeasonOutput(id, orderInTVShow, tvShowId);
+    }
+    
+    public static TVSeason convertToDataFrom(TVSeasonInput inputData, PrimaryKey tvShowForeignKey)
+    {
+        PrimaryKey placeholderKey = new PrimaryKey(0);
+        
+        int orderInTVShow = inputData.getOrderInTVShow();
+        
+        return new TVSeason(placeholderKey, orderInTVShow, tvShowForeignKey);
+    }
+    
+    public static TVSeason convertToDataFrom(TVSeasonOutput outputData)
+    {
+        PrimaryKey primaryKey = new PrimaryKey(outputData.getId());       
+        
+        int orderInTVShow = outputData.getOrderInTVShow();
+        
+        PrimaryKey tvShowForeignKey = new PrimaryKey(outputData.getTVShowId());
+        
+        return new TVSeason(primaryKey, orderInTVShow, tvShowForeignKey);
+    }
 }
