38c76a9a7a06020af6e78f55cd9e9872f9ea3260
Jan Dostal <jan.dostal@tul.cz>
2023-06-21 17:18:21 +0200

    commented main testing methods in unit testing classes

--- star-wars-media-content-management/src/main/java/tests/mainmethods/OutputModelsTest.java
+++ star-wars-media-content-management/src/main/java/tests/mainmethods/OutputModelsTest.java
@@ -12,44 +12,43 @@ public class OutputModelsTest
     //testing main
     public static void main(String[] args) 
     {
-        //movie test
-        System.out.println();
-        System.out.println("movie test");
-        System.out.println();
-        
-        System.out.println("Movie record size: " + MovieOutput.MOVIE_RECORD_SIZE + " B");
-        
-        //tv episode test
-        System.out.println();
-        System.out.println("tv episode test");
-        System.out.println();
-        System.out.println("TV episode record size: " + TVEpisodeOutput.TV_EPISODE_RECORD_SIZE + " B");
-        
-        //tv season test
-        System.out.println();
-        System.out.println("tv season test");
-        System.out.println();
-        System.out.println("TV season record size: " + TVSeasonOutput.TV_SEASON_RECORD_SIZE + " B");
-        
-        //tv show test
-        System.out.println();
-        System.out.println("tv show test");
-        System.out.println();
-        System.out.println("TV show record size: " + TVShowOutput.TV_SHOW_RECORD_SIZE + " B");
-        
-        // null data attributes test
-        System.out.println();
-        System.out.println("null data attributes test");
-        System.out.println();
-        
-        TVShowOutput show = new TVShowOutput(2, null, 2, "    ");
-        System.out.println(show);
-        
-        TVEpisodeOutput episode = new TVEpisodeOutput(2, 3, "", 2, "   ", null, 2, 2);
-        System.out.println(episode);
-        
-        MovieOutput movie = new MovieOutput(2, 3, null, 3, "    ", "", 2, null);
-        System.out.println(movie);
+//        //movie test
+//        System.out.println();
+//        System.out.println("movie test");
+//        System.out.println();
+//        
+//        System.out.println("Movie record size: " + MovieOutput.MOVIE_RECORD_SIZE + " B");
+//        
+//        //tv episode test
+//        System.out.println();
+//        System.out.println("tv episode test");
+//        System.out.println();
+//        System.out.println("TV episode record size: " + TVEpisodeOutput.TV_EPISODE_RECORD_SIZE + " B");
+//        
+//        //tv season test
+//        System.out.println();
+//        System.out.println("tv season test");
+//        System.out.println();
+//        System.out.println("TV season record size: " + TVSeasonOutput.TV_SEASON_RECORD_SIZE + " B");
+//        
+//        //tv show test
+//        System.out.println();
+//        System.out.println("tv show test");
+//        System.out.println();
+//        System.out.println("TV show record size: " + TVShowOutput.TV_SHOW_RECORD_SIZE + " B");
+//        
+//        // null data attributes test
+//        System.out.println();
+//        System.out.println("null data attributes test");
+//        System.out.println();
+//        
+//        TVShowOutput show = new TVShowOutput(2, null, 2, "    ");
+//        System.out.println(show);
+//        
+//        TVEpisodeOutput episode = new TVEpisodeOutput(2, 3, "", 2, "   ", null, 2, 2);
+//        System.out.println(episode);
+//        
+//        MovieOutput movie = new MovieOutput(2, 3, null, 3, "    ", "", 2, null);
+//        System.out.println(movie);
     }
-    
 }
